b16991bd3c5eaafb4a73fc6b12868ca72d0c1990
Tejas Bondre <tejasbondre@gmail.com>
2014-11-06 18:52:32 -0500

    OpenGL functions added. Simplest basic rendering accomplished

--- swarm.cpp
+++ swarm.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
 #include <glm/glm.hpp>
 #include "math.h"
-#include <iostream>
+#include <GL/glut.h>
 
 using namespace glm;
 
@@ -13,14 +14,52 @@ class Boid {
     }
 };
 
+const unsigned int width = 512;
+const unsigned int height = 512;
+Boid* b;
+void render(void);
+
 int main (int argc, char *argv[]) {
-    dvec3 position = dvec3(rand()%512, rand()%512, rand()%512);
-    dvec3 velocity = dvec3(rand()%512, rand()%512, rand()%512);
-    Boid* b = new Boid(position, velocity);
-    for (int i=0; i<10; i++) {
-        b->updatePos();
-        std::cout <<"position = ("<< b->pos.x <<", "<< b->pos.y <<", "<< b->pos.z <<
-          ")   and   velocity = ("<< b->vel.x <<", "<< b->vel.y <<", "<< b->vel.z <<")"<< std::endl;
-    }
+    dvec3 position = dvec3(255, 255, 255);
+    dvec3 velocity = dvec3(rand()%2, rand()%2, rand()%2);
+    b = new Boid(position, velocity);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(width, height);
+    glutCreateWindow("swarm");
+    glutDisplayFunc(render);
+    glutIdleFunc(render);
+    glutMainLoop();
+
     return 0;
+}
+
+
+void render(void) {
+    unsigned int image_plane[height][width][3];
+    for (int x=0; x<width; x++) { 
+        for (int y=0; y<height; y++) { 
+            image_plane[y][x][0]=0;
+            image_plane[y][x][1]=0;
+            image_plane[y][x][2]=0;
+        }
+    }
+
+    b->updatePos();
+    std::cout <<"position = ("<< b->pos.x <<", "<< b->pos.y <<", "<< b->pos.z <<
+      ")   and   velocity = ("<< b->vel.x <<", "<< b->vel.y <<", "<< b->vel.z <<")"<< std::endl;
+
+    for (int x=0; x<512; x++) {
+        for (int y=0; y<512; y++) {
+            if ((x==b->pos.x) && (y==b->pos.y)) {
+                image_plane[y][x][0] = (unsigned int) (trunc (255*255*255*(255*0.9)));
+                image_plane[y][x][1] = (unsigned int) (trunc (255*255*255*(255*0.9)));
+                image_plane[y][x][2] = (unsigned int) (trunc (255*255*255*(255*0.9)));
+            }
+        }
+    }
+    glDrawPixels( width, height, GL_RGB, GL_UNSIGNED_INT, image_plane );
+    glutSwapBuffers();
 }
