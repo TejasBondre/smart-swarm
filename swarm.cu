00cf7f1f4a9cb464a6797e26f1d782850f0f2a27
Tejas Bondre <tejasbondre@gmail.com>
2014-11-29 18:30:18 -0500

    Same program now in GPGPU with CUDA. Used arrays for efficient memory transfer between CPU and GPU. Need to debug Alignment algorithm.

--- swarm.cu
+++ swarm.cu
@@ -5,119 +5,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define numBoids 100000
+#define numThreads 1000
+#define timeStep 1
+#define width 512
+#define height 512
+#define boidSize 1
 using namespace std;
-const int numBoids = 100;
-const unsigned int width = 512;
-const unsigned int height = 512;
-double timeStep = 0.001;
+
 GLfloat ctrlpoints[numBoids][3];
-int boidSize = 5;
-double camzz = 1024.0;
-double cohesionDist=100, cohesionRatio=0.01;
-double alignmentDist=70, alignmentRatio=0.02; 
-double separationDist=30, separationRatio=-0.005; 
-
-/*||||| insert comment here to describe next logical code block. if no description |||||*/
-/*||||| - yet, then retain this comment as a separator to make code reading easier |||||*/
-
-float pos[numBoids][3];
-float vel[numBoids][3];
+float h_pos[numBoids][3];
+float h_vel[numBoids][3];
 float col[numBoids][3];
-float center[3];
-float temp[3];
-
-inline void negate(float ans[3], float x[3]) { ans[0]=0-x[0]; ans[1]=0-x[1]; ans[2]=0-x[2]; }
-inline void vAdd(float ans[3], float x[3], float y[3]) { ans[0]=x[0]+y[0]; ans[1]=x[1]+y[1]; ans[2]=x[2]+y[2]; }
-inline void vSubt(float ans[3], float x[3], float y[3]) { ans[0]=x[0]-y[0]; ans[1]=x[1]-y[1]; ans[2]=x[2]-y[2]; }
-inline void vMult(float ans[3], float x[3], float scalar) { ans[0]=x[0]*scalar; ans[1]=x[1]*scalar; ans[2]=x[2]*scalar; }
-inline float leng(float x[3]) { return sqrt ( (x[0]*x[0]) + (x[1]*x[1]) + (x[2]*x[2]) ); }
+const int DATA_SIZE_BYTES = 3* numBoids * sizeof(float);
+float * d_pos; 
+float * d_vel;
+float camzz = 1024.0;
 
 /*||||| insert comment here to describe next logical code block. if no description |||||*/
 /*||||| - yet, then retain this comment as a separator to make code reading easier |||||*/
-	
-void getNeighbours(float position[3], int radius, int neighbours[numBoids]) {
-	int ii=0;
-	for (int i = 0; i < numBoids; i++) {
-		float posOther[3] = {pos[i][0], pos[i][1], pos[i][2]};
-		vSubt(temp, posOther, position);
-		float distanceBetween = leng (temp);
-		if (distanceBetween < radius) {
-			neighbours[ii] = i;
-			ii++;
+
+__global__ void updateForces(float * d_pos, float * d_vel) {
+	int k = ((blockIdx.x)*numThreads) + threadIdx.x;
+	float force[3]={0,0,0};
+	float centerCohesion[3]={0,0,0}; float centerSeparation[3]={0,0,0}; 
+	int neighbourCohesion=0; int neighbourSeparation=0;
+	//++++ COHESION and SEPARATION ++++++++++++++++++++++++++++++++++++++++++++++++
+	int cohesionDist=100; float cohesionRatio=1.0;
+	int separationDist=30; float separationRatio=0.0005;
+	for(int i=0; i<numBoids; i++) {
+		float posOther[3] = {d_pos[i*3+0], d_pos[i*3+1], d_pos[i*3+2]};
+		posOther[0]-=d_pos[k*3+0]; posOther[1]-=d_pos[k*3+1]; posOther[2]-=d_pos[k*3+2];
+		float distanceBetween = ( (posOther[0]*posOther[0]) + (posOther[1]*posOther[1]) + (posOther[2]*posOther[2]) );
+		if(distanceBetween < cohesionDist*cohesionDist) {
+			centerCohesion[0]+=d_pos[i*3+0]; centerCohesion[1]+=d_pos[i*3+1]; centerCohesion[2]+=d_pos[i*3+2]; 
+			neighbourCohesion++;
+		}
+		if (distanceBetween < separationDist*separationDist) {
+			centerSeparation[0]+=d_pos[i*3+0]; centerSeparation[1]+=d_pos[i*3+1]; centerSeparation[2]+=d_pos[i*3+2]; 
+			neighbourSeparation++;
 		}
 	}
-	for(int i=ii; i<numBoids; i++) neighbours[i]=-1;
-}
-
-void updatePos(int k) { 
-	vMult(temp, vel[k], timeStep);
-	vAdd(pos[k], pos[k], temp);
-}
-
-void updateForces(int k) {
-	float force[3];
-	float distanceFromCenter[3];
-	int neighbours[numBoids];
-	//++++ COHESION +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	vMult(center, center, 0);
-	getNeighbours(pos[k], cohesionDist, neighbours);
-	double neighbourSize;
-	for (int j = 0; j< numBoids; j++) {
-		if (neighbours[j] >= 0)
-			vAdd(center, center, pos[neighbours[j]]);
-		else { neighbourSize = j; break; }
-	}
-	vMult(center, center, (1.0 / (float)neighbourSize));
-	vSubt(distanceFromCenter, center, pos[k]);
-	vMult(distanceFromCenter, distanceFromCenter, cohesionRatio);
-	vAdd(force, force, distanceFromCenter);
-
-	//++++ SEPARATION +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	vMult(center, center, 0);
-	getNeighbours(pos[k], separationDist, neighbours);
-	for (int j = 0; j< numBoids; j++) {
-		if (neighbours[j] >= 0)
-			vAdd(center, center, pos[neighbours[j]]);
-		else { neighbourSize = j; break; }
-	}
-	vMult(center, center, (1.0/ (float)neighbourSize));
-	vSubt(distanceFromCenter, center, pos[k]);
-	vMult(distanceFromCenter, distanceFromCenter, separationRatio);
-	vAdd(force, force, distanceFromCenter);
-
-
+	centerCohesion[0]/=(float)neighbourCohesion; centerCohesion[1]/=(float)neighbourCohesion; centerCohesion[2]/=(float)neighbourCohesion; 
+	centerSeparation[0]/=(float)neighbourSeparation; centerSeparation[1]/=(float)neighbourSeparation; centerSeparation[2]/=(float)neighbourSeparation; 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	vMult(force, force, timeStep);
-	vAdd(vel[k], vel[k], force);
+	force[0] += ((centerCohesion[0]-d_pos[k*3+0])*cohesionRatio) + ((centerSeparation[0]-d_pos[k*3+0])*separationRatio);
+	force[1] += ((centerCohesion[1]-d_pos[k*3+1])*cohesionRatio) + ((centerSeparation[1]-d_pos[k*3+1])*separationRatio);
+	force[2] += ((centerCohesion[2]-d_pos[k*3+2])*cohesionRatio) + ((centerSeparation[2]-d_pos[k*3+2])*separationRatio);
+	d_vel[k*3+0] += force[0]*timeStep;
+	d_vel[k*3+1] += force[1]*timeStep;
+	d_vel[k*3+2] += force[2]*timeStep;
 }
 
-void alignVel(int k) {
+__global__ void alignVel(float * d_pos, float * d_vel) {
+	int k = ((blockIdx.x)*numThreads) + threadIdx.x;
+/*	float alignmentDist = 70;
+	float alignmentRatio = 0.002;
 	float avgVel[3];
-	float velocityDifference[3];
-	int neighbours[numBoids];
+	int neighbourSize = 0;
 	//++++ ALIGNMENT ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	getNeighbours(pos[k], alignmentDist, neighbours);
-	double neighbourSize;
-	for (int j = 0; j< numBoids; j++) {
-		if (neighbours[j] >= 0)
-			vAdd(avgVel, avgVel, pos[neighbours[j]]);
-		else { neighbourSize = j; break; }
+	for(int i=0; i<numBoids; i++) {
+		float posOther[3] = {d_pos[i*3+0], d_pos[i*3+1], d_pos[i*3+2]};
+		posOther[0]-=d_pos[k*3+0]; posOther[1]-=d_pos[k*3+1]; posOther[2]-=d_pos[k*3+2];
+		float distanceBetween = ( (posOther[0]*posOther[0]) + (posOther[1]*posOther[1]) + (posOther[2]*posOther[2]) );
+		if(distanceBetween < alignmentDist*alignmentDist) {
+			avgVel[0]+=d_pos[i*3+0]; avgVel[1]+=d_pos[i*3+1]; avgVel[2]+=d_pos[i*3+2]; 
+			neighbourSize++;
+		}
 	}
-	vMult(avgVel, avgVel, (1.0 / (float)neighbourSize));
-	vSubt(velocityDifference, avgVel, vel[k]);
-	vMult(velocityDifference, velocityDifference, alignmentRatio);
-	vAdd(vel[k], vel[k], velocityDifference);
-
-	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	updatePos(k);
+	avgVel[0]/=(float)neighbourSize; avgVel[1]/=(float)neighbourSize; avgVel[2]/=(float)neighbourSize; 
+	d_vel[k*3+0] += (avgVel[0] - d_vel[k*3+0]) * alignmentRatio;
+	d_vel[k*3+1] += (avgVel[1] - d_vel[k*3+1]) * alignmentRatio;
+	d_vel[k*3+2] += (avgVel[2] - d_vel[k*3+2]) * alignmentRatio;
+*/	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	d_pos[k*3+0] += d_vel[k*3+0]*timeStep;
+	d_pos[k*3+1] += d_vel[k*3+1]*timeStep;
+	d_pos[k*3+2] += d_vel[k*3+2]*timeStep;
 }
 
 /*||||| insert comment here to describe next logical code block. if no description |||||*/
 /*||||| - yet, then retain this comment as a separator to make code reading easier |||||*/
 
 void update(void) {
-	for (int i = 0; i<numBoids; i++) updateForces(i);
-	for (int i = 0; i<numBoids; i++) alignVel(i);
+	updateForces<<<(numBoids/numThreads), numThreads>>>(d_pos, d_vel);
+	alignVel<<<(numBoids/numThreads), numThreads>>>(d_pos, d_vel);
+	cudaMemcpy(h_pos, d_pos, DATA_SIZE_BYTES, cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_vel, d_vel, DATA_SIZE_BYTES, cudaMemcpyDeviceToHost);
 	glutPostRedisplay();
 }
 
@@ -129,9 +102,9 @@ void render(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	for (int i = 0; i<numBoids; i++) {
-		ctrlpoints[i][0] = (((int)(pos[i][0])) % 512) - 256;  if(ctrlpoints[i][0] < -256.0) ctrlpoints[i][0]=ctrlpoints[i][0] +512.0;
-		ctrlpoints[i][1] = (((int)(pos[i][1])) % 512) - 256;  if(ctrlpoints[i][1] < -256.0) ctrlpoints[i][1]=ctrlpoints[i][1] +512.0;
-		ctrlpoints[i][2] = ((int)(pos[i][2])) % 512;          if(ctrlpoints[i][2] < 0)      ctrlpoints[i][2]=ctrlpoints[i][2] +512.0;
+		ctrlpoints[i][0] = (((int)(h_pos[i][0])) % 512) - 256;  if(ctrlpoints[i][0] < -256.0) ctrlpoints[i][0]=ctrlpoints[i][0] +512.0;
+		ctrlpoints[i][1] = (((int)(h_pos[i][1])) % 512) - 256;  if(ctrlpoints[i][1] < -256.0) ctrlpoints[i][1]=ctrlpoints[i][1] +512.0;
+		ctrlpoints[i][2] = ((int)(h_pos[i][2])) % 512;          if(ctrlpoints[i][2] < 0)      ctrlpoints[i][2]=ctrlpoints[i][2] +512.0;
 
 		glColor3f(col[i][0], col[i][1], col[i][2]);
 		glBegin(GL_TRIANGLES);
@@ -170,64 +143,66 @@ void render(void) {
 /*||||| insert comment here to describe next logical code block. if no description |||||*/
 /*||||| - yet, then retain this comment as a separator to make code reading easier |||||*/
 
-double xDeltaAngle = 0.0;
-double yDeltaAngle = 0.0;
+float xDeltaAngle = 0.0;
+float yDeltaAngle = 0.0;
 int isDragging = 0;
 int xDragStart = 0;
 int yDragStart = 0;
+
 void mouseMove(int xx, int yy) {
-    if (isDragging) {
-        xDeltaAngle = (xx - xDragStart) * 0.5;
-        yDeltaAngle = (yy - yDragStart) * 0.5;
-    }
+	if (isDragging) {
+		xDeltaAngle = (xx - xDragStart) * 0.5;
+		yDeltaAngle = (yy - yDragStart) * 0.5;
+	}
 }
+
 void mouseButton(int button, int state, int xx, int yy) {
-    if (button == GLUT_LEFT_BUTTON) {
-        if (state == GLUT_DOWN) {
-            isDragging = 1;
-            xDragStart = xx;
-            yDragStart = yy;
-        }
-        else  {
-            glMatrixMode(GL_MODELVIEW);
-            glTranslatef(0, 0, 255);
+	if (button == GLUT_LEFT_BUTTON) {
+		if (state == GLUT_DOWN) {
+			isDragging = 1;
+			xDragStart = xx;
+			yDragStart = yy;
+		}
+		else  {
+			glMatrixMode(GL_MODELVIEW);
+			glTranslatef(0, 0, 255);
 			glRotatef(xDeltaAngle, 0, 1, 0);
 			glRotatef(yDeltaAngle, 1, 0, 0);
 			glTranslatef(0, 0, -255);
-            isDragging = 0;
-        }
-    }
-    if (button == 3) { camzz = camzz - 15.0; }
-    if (button == 4) { camzz = camzz + 15.0; }
+			isDragging = 0;
+		}
+	}
+	if (button == 3) { camzz = camzz - 15.0; }
+	if (button == 4) { camzz = camzz + 15.0; }
 }
 
 void pressKey(int key, int xx, int yy) {
-    switch (key) {
-        case GLUT_KEY_UP : 
-            glMatrixMode(GL_MODELVIEW);
-            glTranslatef(0, 0, 255);
+	switch (key) {
+		case GLUT_KEY_UP : 
+			glMatrixMode(GL_MODELVIEW);
+			glTranslatef(0, 0, 255);
 			glRotatef(9, 1, 0, 0);
 			glTranslatef(0, 0, -255);
-        break;
-        case GLUT_KEY_DOWN :
-            glMatrixMode(GL_MODELVIEW);
-            glTranslatef(0, 0, 255);
+			break;
+		case GLUT_KEY_DOWN :
+			glMatrixMode(GL_MODELVIEW);
+			glTranslatef(0, 0, 255);
 			glRotatef(-9, 1, 0, 0);
 			glTranslatef(0, 0, -255);
-        break;
-        case GLUT_KEY_LEFT : 
-            glMatrixMode(GL_MODELVIEW);
-            glTranslatef(0, 0, 255);
+			break;
+		case GLUT_KEY_LEFT : 
+			glMatrixMode(GL_MODELVIEW);
+			glTranslatef(0, 0, 255);
 			glRotatef(9, 0, 1, 0);
 			glTranslatef(0, 0, -255);
-        break;
-        case GLUT_KEY_RIGHT : 
-            glMatrixMode(GL_MODELVIEW);
-            glTranslatef(0, 0, 255);
+			break;
+		case GLUT_KEY_RIGHT : 
+			glMatrixMode(GL_MODELVIEW);
+			glTranslatef(0, 0, 255);
 			glRotatef(-9, 0, 1, 0);
 			glTranslatef(0, 0, -255);
-        break;
-    }
+			break;
+	}
 } 
 
 /*||||| insert comment here to describe next logical code block. if no description |||||*/
@@ -235,10 +210,10 @@ void pressKey(int key, int xx, int yy) {
 
 int main(int argc, char *argv[]) {
 	srand (time(NULL));
-	for (int i = 0; i<numBoids; i++) {
-		pos[i][0]=rand()%512;  pos[i][1]=rand()%512;  pos[i][2]=rand()%512;
-		vel[i][0]=((rand()%4)-2);  vel[i][1]=((rand()%4)-2);  vel[i][2]=((rand()%4)-2);
-		col[i][0]=((rand()%100)/100.0);  col[i][1]=((rand()%100)/100.0);  col[i][2]=((rand()%100)/100.0);
+	for (int i = 0; i<numBoids; i++) for (int j=0; j<3; j++) {
+		h_pos[i][j] = rand()%512; 
+		h_vel[i][j] = ((rand()%4)-2); 
+		col[i][j] = ((rand()%100)/100.0); 
 	}
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -252,7 +227,14 @@ int main(int argc, char *argv[]) {
 	glutMouseFunc(mouseButton);
 	glutMotionFunc(mouseMove);
 	glutSpecialFunc(pressKey);
+
+	cudaMalloc((void**) &d_pos, DATA_SIZE_BYTES);
+	cudaMalloc((void**) &d_vel, DATA_SIZE_BYTES);
+	cudaMemcpy(d_pos, h_pos, DATA_SIZE_BYTES, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_vel, h_vel, DATA_SIZE_BYTES, cudaMemcpyHostToDevice);
 	glutMainLoop();
+	cudaFree(d_pos);
+	cudaFree(d_vel);
 	return 0;
 }
 
